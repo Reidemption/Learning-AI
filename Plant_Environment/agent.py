0495487a6f2cd86e32c498ab67919b46ae5f0258
Reidemption <75095757+Reidemption@users.noreply.github.com>
2022-11-16 12:26:14 -0700

    just need to fix water levels

---
+++ Plant_Environment/agent.py
@@ -0,0 +1,35 @@
+import env
+import random
+import copy 
+  
+LIMIT = 10
+class Agent:
+  def __init__(self, seed):
+    self.model = env.Plant(seed)
+    
+  # random action
+  def decideAction(self, percepts):
+    self.model.updatePercepts(percepts)
+    actions = self.model.getLegalActions()
+    action = random.choice(actions)
+    return action
+  
+def main():
+  seed = 1234567890
+  environment = env.Plant(seed)
+  agent = Agent(seed)
+  
+  while not environment.done():
+    percepts = environment.getPercepts()
+    print('percepts:', percepts)
+    action = agent.decideAction(percepts)
+    print('action:', action)
+    print('\n')
+    environment.applyAction(action)
+    if environment.GoalTest():
+      print('Goal Test Passed')
+      break
+  return
+
+
+main()
---
+++ Plant_Environment/env.py
@@ -0,0 +1,95 @@
+import random
+import copy
+
+class Plant:
+  def __init__(self, seed):
+    self.plant = {
+      'time': 0,
+      'days': 0,
+      'water_levels': self.initializePlant(seed),
+      'alive': True,
+      'soil_quality':self.initializePlant(seed),
+      'consecutive_light_days': 0,
+      'light_levels': .8,
+      'inLight': False,
+    }
+    
+  def initializePlant(self, seed):
+    random.seed(seed)
+    water_or_soil = random.uniform(0,1)
+    while water_or_soil < 0.3 and water_or_soil > 0.8:
+      water_or_soil = random.uniform(0,1)
+    return water_or_soil
+  
+  def updatePercepts(self, state):
+    self.plant = state
+    
+  def getPercepts(self):
+    return self.plant
+  
+  def getLegalActions(self):
+    if self.plant['days'] >= 30 or not self.plant['alive']:
+      return
+    return ['water', 'wait', 'change soil', 'light', 'remove light']
+  
+  def changeSoil(self):
+    self.plant['soil_quality'] = .8
+    
+  def moveIntoLight(self):
+    self.plant['inLight'] = True
+    
+  def removeFromLight(self):
+    self.plant['inLight'] = False
+  
+  def applyAction(self, action):
+    if action == 'water':
+      self.plant['water_levels'] += 0.3
+    elif action == 'wait':
+      pass
+    elif action == 'change soil':
+      self.changeSoil()
+    elif action == 'light':
+      self.moveIntoLight()
+    elif action == 'remove light':
+      self.removeFromLight()
+    else:
+      print("Invalid action:", action)
+    self.agePlant()
+    
+      
+  def Result(self, action):
+    pass
+  
+  def GoalTest(self):
+    if self.plant['days'] == 30 and self.plant['alive'] == False:
+      return False
+    return True
+    
+  def done(self):
+    if self.GoalTest():
+      print("Plant simulation has ended with the plant alive") if self.plant['alive'] else print("Plant simulation has ended with the plant dead")
+      return True
+    return False
+  
+  def plantAlive(self):
+    if self.plant['water_levels'] <= 0.2 or self.plant['soil_quality'] <= .1 or self.plant['light_levels'] <= 0:
+      self.plant['alive'] = False
+    return self.plant['alive']
+  
+  def Utility(self):
+    pass
+  
+  def agePlant(self):
+    if self.plant['time'] <= 17 and self.plant['time'] >= 7:
+      self.plant['light_levels'] += 0.727272
+    if self.plant['time'] >= 23:
+      self.plant['time'] = 0
+      self.plant['days'] += 1
+    else:
+      self.plant['time'] += 1
+    self.plant['water_levels'] -= 0.1
+    self.plant['soil_quality'] -= 0.0012987
+    self.plant['light_levels'] -= 0.363636
+    self.plantAlive()
+    
+    
